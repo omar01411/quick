feac905f9f58934e6d587bab381e055df4a58e62
Omar Taha <o.taha@glcpaints.com>
2025-11-12 12:30:11 +0200

    new

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"d2913632a4578ee4d0b8b1c4a69888c8a0672c
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "811521613"
+    serviceWorkerVersion: "3817922267"
   }
 });
